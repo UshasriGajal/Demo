2800e84e8cf0be893abd27bff0a01b13afbf2bb3
UshasriGajal <126794491+UshasriGajal@users.noreply.github.com>
2023-10-12 21:09:13 +0530

    Delete practice.js

--- practice.js
+++
@@ -1,7 +0,0 @@
-function mail(){
-   var email = document.getElementById.value;
-   var password = document.getElementById.value;
-
-   localStorage.setItem('userEmail',email);
-   localStorage.setItem('userpassword',password);
-}
